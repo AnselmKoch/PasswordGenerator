134a08a24cdf51a069346bc565979c591855dc4c
BuildTools <unconfigured@null.spigotmc.org>
2021-02-21 16:43:32 +0100

    Made it impossible for the password to contain two of the same characters

--- src/me/marselm/password/Password.java
+++ src/me/marselm/password/Password.java
@@ -7,11 +7,13 @@ public class Password {
     public String password = "";
     public String characters = "qwertzuiopasdfghjklyxcvbnm1234567890!§$%&/()=?{[]}´`*~+'#;,:.--<>^°|";
     public int length = 32;
+    public char randomChar;
 
     public Password() {
         while(password.length() < length) {
-            char randomChar = characters.charAt(ThreadLocalRandom.current().nextInt(0, characters.length()));
-            password += randomChar;
+            while (!password.contains(Character.toString(randomChar = characters.charAt(ThreadLocalRandom.current().nextInt(0, characters.length()))))) {
+                password += randomChar;
+            }
         }
         System.out.println(password);
     }
